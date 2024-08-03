4e0f644cc3f6122114545886cd54b4b393900668
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-08-03 23:54:23 +0000

    Add load_heritage_data fx to data management

--- src/data_management.py
+++ src/data_management.py
@@ -15,5 +15,11 @@ def load_cleaned_house_prices_data():
     return df
 
 
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def load_heritage_data():
+    df_h = pd.read_csv("inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/inherited_houses.csv")
+    return df_h
+    
+
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
