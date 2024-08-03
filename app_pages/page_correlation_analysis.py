3aa8375b077dfc9b45c0a4fa302e9832c83a82de
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-08-03 14:31:26 +0000

    Add code for data visualizations section of corr anal page

--- app_pages/page_correlation_analysis.py
+++ app_pages/page_correlation_analysis.py
@@ -24,7 +24,7 @@ def page_correlation_analysis_body():
                      'YearBuilt',
                      'YearRemodAdd']
 
-    st.write("### Sale Price Correlation Analysis")
+    st.write("### Property Sale Price Correlation Analysis")
     st.success(
         f"* The client is interested in discovering\
             how the house attributes correlate with the sale price.\
@@ -33,7 +33,7 @@ def page_correlation_analysis_body():
     )
 
     # inspect data
-    if st.checkbox("Inspect Customer Base"):
+    if st.checkbox("Inspect Sale Price Dataset"):
         st.write(
             f"* The dataset has {df.shape[0]} rows and {df.shape[1]} columns, "
             f"find below the first 10 rows.")
@@ -45,87 +45,96 @@ def page_correlation_analysis_body():
     # Correlation Study Summary
     st.write(
         f"* A correlation study was conducted in the notebook to better understand how "
-        f"the variables are correlated to Churn levels. \n"
+        f"the variables are correlated to the house sale prices. \n"
         f"The most correlated variable are: **{vars_to_study}**"
     )
 
     # Text based on "02 - Churned Customer Study" notebook - "Conclusions and Next steps" section
-    st.info(
-        f"The correlation indications and plots below interpretation converge. "
-        f"It is indicated that: \n"
-        f"* A churned customer typically has a month-to-month contract \n"
-        f"* A churned customer typically has fibre optic. \n"
-        f"* A churned customer typically doesn't have tech support. \n"
-        f"* A churned customer doesn't have online security. \n"
-        f"* A churned customer typically has low tenure levels. \n"
-    )
+    # st.info(
+    #     f"The correlation indications and plots below interpretation converge. "
+    #     f"It is indicated that: \n"
+    #     f"* A churned customer typically has a month-to-month contract \n"
+    #     f"* A churned customer typically has fibre optic. \n"
+    #     f"* A churned customer typically doesn't have tech support. \n"
+    #     f"* A churned customer doesn't have online security. \n"
+    #     f"* A churned customer typically has low tenure levels. \n"
+    # )
 
-    # Code copied from "02 - Churned Customer Study" notebook - "EDA on selected variables" section
-    df_eda = df.filter(vars_to_study + ['Churn'])
+    df_eda = df.filter(vars_to_study + ['SalePrice'])
+    target_var = 'SalePrice'
+
+    st.write("#### Data Visualizations")
+    # Distribution of target variable
+    if st.checkbox("Distribution of Sale Prices"):
+        plot_target_hist(df_eda, target_var) 
 
     # Individual plots per variable
-    if st.checkbox("Churn Levels per Variable"):
-        churn_level_per_variable(df_eda)
+    if st.checkbox("Sale Price per Correlating Variable"):
+        sale_price_per_variable(df_eda)
 
     # Parallel plot
-    if st.checkbox("Parallel Plot"):
-        st.write(
-            f"* Information in yellow indicates the profile from a churned customer")
-        parallel_plot_churn(df_eda)
+    # if st.checkbox("Parallel Plot"):
+    #     st.write(
+    #         f"* Information in yellow indicates the profile from a churned customer")
+    #     parallel_plot_churn(df_eda)
 
+def plot_target_hist(df, target_var):
+  """
+  Function to plot a histogram of the target and
+  save the figure to folder.
+  """
+  plt.figure(figsize=(12, 5))
+  sns.histplot(data=df, x=target_var, kde=True)
+  plt.title(f"Distribution of {target_var}", fontsize=20)
+  st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
 
-# function created using "02 - Churned Customer Study" notebook code - "Variables Distribution by Churn" section
-def churn_level_per_variable(df_eda):
-    target_var = 'Churn'
+# function created using "03_correlation_study" notebook code - "Distribution of SalePrice" section
+def sale_price_per_variable(df_eda):
+    target_var = 'SalePrice'
+    # from correlation study notebook
+    vars_to_study = ['1stFlrSF',
+                     'GarageArea',
+                     'GrLivArea',
+                     'KitchenQual_TA',
+                     'OverallQual',
+                     'TotalBsmtSF',
+                     'YearBuilt',
+                     'YearRemodAdd']
 
-    for col in df_eda.drop([target_var], axis=1).columns.to_list():
-        if df_eda[col].dtype == 'object':
-            plot_categorical(df_eda, col, target_var)
+    for col in vars_to_study:
+        if len(df_eda[col].unique()) <= 10:
+            corr_box_plot(df_eda, col, target_var)
+            print("\n\n")
         else:
-            plot_numerical(df_eda, col, target_var)
+            if col in time:
+                corr_line_plot(df_eda, col, target_var)
+                print("\n\n")
+            else:
+                corr_lm_plot(df_eda, col, target_var)
+                print("\n\n")
 
+def corr_line_plot(df, col, target_var):
+  """
+  Line plots of target variable vs time variables (years)
+  """
+  fig, axes = plt.subplots(figsize=(10, 5))
+  sns.lineplot(data=df, x=col, y=target_var)
+  plt.title(f"{col}", fontsize=20, y=1.05)
+  st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
 
-# code copied from "02 - Churned Customer Study" notebook - "Variables Distribution by Churn" section
-def plot_categorical(df, col, target_var):
-    fig, axes = plt.subplots(figsize=(12, 5))
-    sns.countplot(data=df, x=col, hue=target_var,
-                  order=df[col].value_counts().index)
-    plt.xticks(rotation=90)
-    plt.title(f"{col}", fontsize=20, y=1.05)
-    st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
+def corr_box_plot(df, col, target_var):
+  """
+  Box plots of target variable vs categorical variables
+  """
+  fig, axes = plt.subplots(figsize=(10, 5))
+  sns.boxplot(data=df, x=col, y=target_var) 
+  plt.title(f"{col}", fontsize=20, y=1.05)
+  st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
 
-
-# code copied from "02 - Churned Customer Study" notebook - "Variables Distribution by Churn" section
-def plot_numerical(df, col, target_var):
-    fig, axes = plt.subplots(figsize=(8, 5))
-    sns.histplot(data=df, x=col, hue=target_var, kde=True, element="step")
-    plt.title(f"{col}", fontsize=20, y=1.05)
-    st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
-
-
-# function created using "02 - Churned Customer Study" notebook code - Parallel Plot section
-def parallel_plot_churn(df_eda):
-
-    # hard coded from "disc.binner_dict_['tenure']"" result,
-    tenure_map = [-np.Inf, 6, 12, 18, 24, np.Inf]
-    # found at "02 - Churned Customer Study" notebook
-    # under "Parallel Plot" section
-    disc = ArbitraryDiscretiser(binning_dict={'tenure': tenure_map})
-    df_parallel = disc.fit_transform(df_eda)
-
-    n_classes = len(tenure_map) - 1
-    classes_ranges = disc.binner_dict_['tenure'][1:-1]
-    LabelsMap = {}
-    for n in range(0, n_classes):
-        if n == 0:
-            LabelsMap[n] = f"<{classes_ranges[0]}"
-        elif n == n_classes-1:
-            LabelsMap[n] = f"+{classes_ranges[-1]}"
-        else:
-            LabelsMap[n] = f"{classes_ranges[n-1]} to {classes_ranges[n]}"
-
-    df_parallel['tenure'] = df_parallel['tenure'].replace(LabelsMap)
-    fig = px.parallel_categories(
-        df_parallel, color="Churn", width=750, height=500)
-    # we use st.plotly_chart() to render, in notebook is fig.show()
-    st.plotly_chart(fig)
+def corr_lm_plot(df, col, target_var):
+  """
+  Linear regression plots of target variable vs continuous features"
+  """
+  sns.lmplot(data=df, x=col, y=target_var, height=6, aspect=1.5)
+  plt.title(f"{col}", fontsize=20, y=1.05)
+  st.pyplot(fig)  # st.pyplot() renders image, in notebook is plt.show()
