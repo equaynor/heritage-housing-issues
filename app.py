2c2b4611b3ccc8d72bf4f63e2b25dd099cbe1de2
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-08-03 14:45:10 +0000

    Add correlation analysis page to app.py

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
+from app_pages.page_correlation_analysis import page_correlation_analysis_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Heritage Housing Sale Price Predictor")
 
 # Add your app pages here using .add_page()
 app.add_page("Project Overview", page_summary_body)
+app.add_page("Correlation Analysis", page_correlation_analysis_body)
 
 
 app.run()  # Run the  app
