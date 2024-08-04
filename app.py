e60c625c6cb8ff870227f7f9a91dd592b351609e
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-08-04 09:13:42 +0000

    Add hypothesis page to app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_correlation_analysis import page_correlation_analysis_body
 from app_pages.page_sale_price_predictor import page_sale_price_predictor_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Heritage Housing Sale Price Predictor")
@@ -13,6 +14,7 @@ app = MultiPage(app_name="Heritage Housing Sale Price Predictor")
 app.add_page("Project Overview", page_summary_body)
 app.add_page("Correlation Analysis", page_correlation_analysis_body)
 app.add_page("Sale Price Prediction", page_sale_price_predictor_body)
+app.add_page("Hypothesis Validation", page_project_hypothesis_body)
 
 
 app.run()  # Run the  app
