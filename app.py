6806c404ff590e0acd315ca3f4e868fff4c98c2d
Emmanuel Quaynor <e.c.quaynor@gmail.com>
2024-08-04 09:43:47 +0000

    Add machine learning model page to app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_correlation_analysis import page_correlation_analysis_body
 from app_pages.page_sale_price_predictor import page_sale_price_predictor_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_predict_price_ml import page_predict_price_ml_body
 
 # Create an instance of the app
 app = MultiPage(app_name="Heritage Housing Sale Price Predictor")
@@ -15,6 +16,7 @@ app.add_page("Project Overview", page_summary_body)
 app.add_page("Correlation Analysis", page_correlation_analysis_body)
 app.add_page("Sale Price Prediction", page_sale_price_predictor_body)
 app.add_page("Hypothesis Validation", page_project_hypothesis_body)
+app.add_page("Machine Learning Model", page_predict_price_ml_body)
 
 
 app.run()  # Run the  app
